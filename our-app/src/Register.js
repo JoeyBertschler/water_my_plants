6931ffb9084dcf3a49124adc259173933a89b900
Matt <matthew-casey@lambdastudents.com>
2021-06-19 12:18:54 -0500

    added register component

---
+++ our-app/src/Register.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Register () {
+        <div>
+            <form>
+                <label>
+                    
+                </label>
+            </form>
+        </div>
+}
+
+export default Register
